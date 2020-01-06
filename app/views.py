5f934a5ad753bb2d98018634efff03db9cfae8db
Nicholas Lamicela <nlamicela@taxcloud.com>
2020-01-06 13:35:56 -0800

    Set up default app and view.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request) -> HttpResponse:
+    return HttpResponse(
+        "Hello, world. You're at the app index."
+    )
 
 # Create your views here.
